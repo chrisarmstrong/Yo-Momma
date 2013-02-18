d3784adc71d2a13714681933ecc896d2138641c0
Chris Armstrong <fatbuoy1@gmail.com>
2013-02-18 13:03:30 +0100

    Seperated style and script files out
    The insults are contained in a JSON object at the top of the yomomma.js
    file

---
+++ yomomma.js
@@ -0,0 +1,48 @@
+var jokes = {
+		    "fat":[
+		      "she expresses her weight in scientific notation",
+		      "when she walked by my window I didn't see the sun for three days",
+		      "she has her own postcode",
+		      "she jumped up in the air and got stuck",
+		      "when she stands up she has to sit down for a break",
+		      "Obi Wan Kenobi said “Yep, that’s a moon”"
+		    ],
+		  "ugly":[
+		      "she had to tie a steak to her neck so the dog wanted to play with her",
+		    "even the tide won't take her out",
+		    "she went into a haunted house and came out with an application",
+		    "her dentist treats her by mail-order"
+		   ],
+		  "stupid":[
+		      "she bought tickets to Xbox Live",
+		      "she studied for a drug test",
+		      "she bought a solar-powered flashlight",
+		      "she got stabbed in a shoot out"
+		  ]
+		};
+
+var categories = Object.keys(jokes);
+		
+function newJoke(cat) {
+  if(cat) {
+    $('.category').text(cat); 
+    for(var i = 0; i < categories.length; i++) {
+      if(categories[i] == cat) {
+          $('.punchline').text(jokes[categories[i]][Math.floor(Math.random()*jokes[categories[i]].length)]);
+      }
+    }
+  } else {
+    var i = Math.floor(Math.random()*categories.length);
+  
+    $('.category').text(categories[i]);
+    $('.punchline').text(jokes[categories[i]][Math.floor(Math.random()*jokes[categories[i]].length)]);
+  }
+
+}
+		
+newJoke();
+		
+$('.new-joke').click(function(){
+   newJoke();
+   return false;
+})
